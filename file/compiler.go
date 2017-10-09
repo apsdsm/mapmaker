3d744d257541d970fd7576c4c8d3e06bc830f167
Nick del Pozo <nickdelpozo@gmail.com>
2017-10-09 15:41:54 +0900

    add neighbors to tiles

--- file/compiler.go
+++ file/compiler.go
@@ -1,3 +1,17 @@
+// Copyright 2017 Nick del Pozo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package file
 
 import (
@@ -8,6 +22,9 @@ import (
 func CompileLevel(metaData *placeholders.Meta, mapData *placeholders.Map, entities *placeholders.EntityCollection) *placeholders.Dungeon {
 	m := placeholders.NewDungeon(mapData.Width, mapData.Height)
 
+	// copy tile data
+	// - set floor tiles as walkable
+	// - set spawn data for tiles
 	for x := 0; x < mapData.Width; x++ {
 		for y := 0; y < mapData.Height; y++ {
 			m.Tiles[x][y].Rune = mapData.Grid[x][y].Rune
@@ -21,13 +38,47 @@ func CompileLevel(metaData *placeholders.Meta, mapData *placeholders.Map, entiti
 		}
 	}
 
+	// calc the neighbors for each tile start at N and go clockwise:
+	// 8 1 2
+	// 7   3
+	// 6 5 4
+	jump := []placeholders.Position{
+		{0, -1},  // N
+		{1, -1},  // NE
+		{1, 0},   // E
+		{1, 1},   // SE
+		{0, 1},   // S
+		{-1, 1},  // SW
+		{-1, 0},  // W
+		{-1, -1}, // NW
+	}
+
+	for x := range m.Tiles {
+		for y := range m.Tiles[x] {
+			for n := 0; n < 8; n++ {
+				nPos := placeholders.Position{x + jump[n].X, y + jump[n].Y}
+
+				// if any part of the coord is out of bounds then it is set as an invalid position
+				if nPos.X < 0 || nPos.Y < 0 || nPos.X >= mapData.Width || nPos.Y >= mapData.Height {
+					nPos = placeholders.Position{-1, -1}
+				}
+
+				m.Tiles[x][y].Neighbors[n] = nPos
+			}
+		}
+	}
+
+	// start position
 	if mapData.StartPosition != nil {
 		m.StartPosition = *mapData.StartPosition
 	}
 
+	// copy meta
 	m.Link = metaData.Link
 	m.Name = metaData.Name
 	m.Desc = metaData.Desc
+
+	// copy entities
 	m.Doors = entities.Doors
 	m.Mobs = entities.Mobs
 	m.Keys = entities.Keys
--- file/compiler_test.go
+++ file/compiler_test.go
@@ -1,9 +1,24 @@
+// Copyright 2017 Nick del Pozo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package file_test
 
 import (
 	. "github.com/apsdsm/mapmaker/file"
 	"github.com/apsdsm/mapmaker/placeholders"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
@@ -35,9 +50,7 @@ var _ = Describe("Compiler", func() {
 
 		// NB: cols/rows are inverted
 		walkable := [][]bool{
-			{false, false, false},
-			{false, true, false},
-			{false, false, false},
+			{false, false, false}, {false, true, false}, {false, false, false},
 		}
 
 		for i := 0; i < len(walkable); i++ {
@@ -84,4 +97,49 @@ var _ = Describe("Compiler", func() {
 
 		Expect(dungeon.StartPosition).To(Equal(placeholders.Position{4, 3}))
 	})
+
+	// 0 1 2
+	// 3 4 5
+	// 6 7 8
+	DescribeTable("it calculates tile neighbors",
+		func(check int, neighbors [8]int) {
+
+			map_source := "../fixtures/maps/meta_and_map.map"
+			ent_source := "../fixtures/makelists/annotated.yaml"
+
+			metaData, mapData := ImportMap(map_source)
+			entities := ImportEntities(ent_source)
+
+			dungeon := CompileLevel(metaData, mapData, entities)
+
+			// tile coords
+			tile := make(map[int]placeholders.Position)
+			tile[-1] = placeholders.Position{-1, -1}
+			tile[0] = placeholders.Position{0, 0}
+			tile[1] = placeholders.Position{1, 0}
+			tile[2] = placeholders.Position{2, 0}
+			tile[3] = placeholders.Position{0, 1}
+			tile[4] = placeholders.Position{1, 1}
+			tile[5] = placeholders.Position{2, 1}
+			tile[6] = placeholders.Position{0, 2}
+			tile[7] = placeholders.Position{1, 2}
+			tile[8] = placeholders.Position{2, 2}
+
+			// check this tile
+			c := dungeon.Tiles[tile[check].X][tile[check].Y]
+
+			for i, n := range neighbors {
+				Expect(c.Neighbors[i]).To(Equal(tile[n]))
+			}
+		},
+		Entry("tile 0", 0, [8]int{-1, -1, 1, 4, 3, -1, -1, -1}),
+		Entry("tile 1", 1, [8]int{-1, -1, 2, 5, 4, 3, 0, -1}),
+		Entry("tile 2", 2, [8]int{-1, -1, -1, -1, 5, 4, 1, -1}),
+		Entry("tile 3", 3, [8]int{0, 1, 4, 7, 6, -1, -1, -1}),
+		Entry("tile 4", 4, [8]int{1, 2, 5, 8, 7, 6, 3, 0}),
+		Entry("tile 5", 5, [8]int{2, -1, -1, -1, 8, 7, 4, 1}),
+		Entry("tile 6", 6, [8]int{3, 4, 7, -1, -1, -1, -1, -1}),
+		Entry("tile 7", 7, [8]int{4, 5, 8, -1, -1, -1, 6, 3}),
+		Entry("tile 8", 8, [8]int{5, -1, -1, -1, -1, -1, 7, 4}),
+	)
 })
